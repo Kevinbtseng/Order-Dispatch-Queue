356e2f5b4ba2f897cef317459f1e38bdf72c1830
kevinbtseng <kevinbtseng@gmail.com>
2026-07-04 18:12:32 -0500

    chore: add Celery app connection setup

---
+++ app/celery_app.py
@@ -0,0 +1,10 @@
+from celery import Celery
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+celery_app = Celery("orders", 
+             broker=os.getenv("CELERY_BROKER_URL"), 
+             backend=os.getenv("CELERY_RESULT_BACKEND"))
+
